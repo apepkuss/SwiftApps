3be6877bb72afde2eeac772c68a1531e36043998
LiuXin <Xin@LiuXins-MacBook-Pro.local>
2015-04-07 18:02:05 -0700

    Cassini code

---
+++ Cassini/Cassini/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by LiuXin on 4/6/15.
+//  Copyright (c) 2015 LiuXin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController {
+            if let identifier = segue.identifier {
+                switch identifier {
+                    case "Earth":
+                        ivc.imageURL = DemoURL.NASA.Earth
+                        ivc.title = "Earth"
+                    case "Cassini":
+                        ivc.imageURL = DemoURL.NASA.Cassini
+                        ivc.title = "Cassini"
+                    case "Saturn":
+                        ivc.imageURL = DemoURL.NASA.Saturn
+                        ivc.title = "Saturn"
+                default: break
+                }
+            }
+        }
+    }
+}
+
